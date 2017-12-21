20a859672be32715b56d68ebeff79ce14ce5f7ce
Spencer Oberstadt <soberstadt@gmail.com>
2017-12-21 11:03:07 -0500

    add method stubs

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # kirby
 
-![jumping picture of Kirby](https://raw.githubusercontent.com/CruGlobal/kirby/master/kirby.png)
+![jumping picture of Kirby](https://raw.githubusercontent.com/CruGlobal/kirby/master/kirby.png)  
 Picture of Kirby from Nintendo's website, so we'll probably get 
-a cease and desist from them or something
+a Cease and Desist from them or something
--- index.js
+++ index.js
@@ -2,6 +2,51 @@
 
 console.log('Loading function');
 
+var clone = true;
+
 exports.handler = function(event, context, callback) {
+    const table = event.table;
+    const uuids = event.uuids.split(/,/);
+
+    if(event["clone"] !== undefined)
+        clone = event.clone;
+
+    p(table);
+    p(uuids);
+
+    connectToDatabases();
+
+    checkTables(table);
+
+    checkRows(table, uuids);
+
+    moveRows(table, uuids);
+
     callback(null, "some success message");
 };
+
+function connectToDatabases() {
+    p('connecting to databases');
+}
+
+function checkTables(table) {
+    p('checking that table exists both places');
+}
+
+function checkRows(table, uuids) {
+    p('checking that the rows exist in master db');
+}
+
+function moveRows(table, uuids) {
+    p('copying rows to slave db');
+    if(!clone)
+        deleteRows(table, uuids);
+}
+
+function deleteRows(table, uuids) {
+    p('deleting rows from master db');
+}
+
+function p(s) {
+    console.log(s)
+}
