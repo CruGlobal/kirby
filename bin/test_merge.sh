e7e033b205a7cca2d395fc0f481f250a50424c17
Spencer Oberstadt <soberstadt@gmail.com>
2019-02-18 16:54:49 -0500

    notify room of failure

--- bin/test_merge.sh
+++ bin/test_merge.sh
@@ -16,6 +16,9 @@ success=$?
 # if merge was unsuccessful, abort the failed merge
 if [ ! $success == 0 ] ; then
   git merge --abort
+  curl -X POST -H "Content-Type: application/json" \
+      -d '{"message":"Failed to merge master into staging on CruGlobal/kirby! Fix it soon before another developer ties to merge their branch into staging!"}' \
+      http://c4e22262d9bcf22b2db37928a534665a.balena-devices.com:8080/hubot/send_to/${NOTIFY_ROOM}
 fi
 
 exit $success
