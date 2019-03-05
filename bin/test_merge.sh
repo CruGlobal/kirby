2cde7d0c727fbda90d44ed7f54a1e44282d6b75e
Spencer Oberstadt <soberstadt@gmail.com>
2019-03-04 23:17:44 -0500

    call new failure webhook

--- bin/test_merge.sh
+++ bin/test_merge.sh
@@ -3,6 +3,8 @@
 # print every command before executing
 set -x
 
+committer_email="$(git log -1 $TRAVIS_COMMIT --pretty="%cE")"
+
 git remote set-branches origin 'staging'
 git fetch origin --depth=50
 git checkout -b staging origin/staging
@@ -17,7 +19,10 @@ success=$?
 if [ ! $success == 0 ] ; then
   git merge --abort
   curl -X POST -H "Content-Type: application/json" \
-      -d '{"message":"Failed to merge master into staging on CruGlobal/kirby! Fix it soon before another developer ties to merge their branch into staging!"}' \
+      -d "{\"repo\":\"${TRAVIS_REPO_SLUG}\",\"author\":\"${committer_email}\"}" \
+      http://c4e22262d9bcf22b2db37928a534665a.balena-devices.com:8080/hubot/failed_merge_check
+  curl -X POST -H "Content-Type: application/json" \
+      -d "{\"message\":\"Failed to merge master into staging on ${TRAVIS_REPO_SLUG}. Fix it soon before another developer ties to merge their branch into staging!\"} " \
       http://c4e22262d9bcf22b2db37928a534665a.balena-devices.com:8080/hubot/send_to/${NOTIFY_ROOM}
 fi
 
