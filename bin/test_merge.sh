3266e0b3ae998d370cdd9f3fdf5312c45e705cad
Spencer Oberstadt <soberstadt@gmail.com>
2019-03-04 23:18:34 -0500

    remove redundant webhook call

--- bin/test_merge.sh
+++ bin/test_merge.sh
@@ -21,9 +21,6 @@ if [ ! $success == 0 ] ; then
   curl -X POST -H "Content-Type: application/json" \
       -d "{\"repo\":\"${TRAVIS_REPO_SLUG}\",\"author\":\"${committer_email}\"}" \
       http://c4e22262d9bcf22b2db37928a534665a.balena-devices.com:8080/hubot/failed_merge_check
-  curl -X POST -H "Content-Type: application/json" \
-      -d "{\"message\":\"Failed to merge master into staging on ${TRAVIS_REPO_SLUG}. Fix it soon before another developer ties to merge their branch into staging!\"} " \
-      http://c4e22262d9bcf22b2db37928a534665a.balena-devices.com:8080/hubot/send_to/${NOTIFY_ROOM}
 fi
 
 exit $success
