5f288eec5715f48d7317695cbb7a8c00d790aad6
Justin Sabelko <justin.sabelko@cru.org>
2023-01-18 15:13:08 -0500

    Fix spacing issue

--- src/index.js
+++ src/index.js
@@ -64,7 +64,7 @@ export const handler = async (event) => {
     console.log('connecting to slave')
     slaveClient = await slavePool.connect()
     await slaveClient.query('SELECT NOW()')
-}
+  }
 
   // checking that table exists both places
   const checkTables = async () => {
