fcd0fe3a6d2ecb79ae069f0f7159cfc9e22a1cbe
Justin Sabelko <justin.sabelko@cru.org>
2023-01-18 17:10:23 -0500

    debug

--- src/index.js
+++ src/index.js
@@ -137,7 +137,7 @@ export const handler = async (event) => {
       if (typeof (v) === 'number') { return v }
       if (v !== null && v.constructor.name === 'Date') { return escape.literal(v.toISOString()) }
       if (v !== null && v.constructor.name === 'Boolean') { return escape.string(v) }
-      console.log(v.constructor.name)
+      if (v !== null) { console.log(v.constructor.name) }
       return escape.literal(v)
     })
   }
